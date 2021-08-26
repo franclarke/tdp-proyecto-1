d3ecb675b9615bbb0e4ad1ce261e4763394c577c
franclarke <franclarke00@hotmail.com>
2021-08-26 10:47:25 -0300

    Agrego stud al main

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -5,14 +5,13 @@ import gui.SimplePresentationScreen;
 
 public class Launcher{
 	
-	private static Student stud = new Student(120547, "Clarke", "Francisco", "franclarke00@hotmail.com", "https://github.com/franclarke", "/images/foto.png");
-	
 	public static void main(String [] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
             	try {
+			Student stud = new Student(120547, "Clarke", "Francisco", "franclarke00@hotmail.com", "https://github.com/franclarke", "/images/foto.png");
             		SimplePresentationScreen frame = new SimplePresentationScreen(stud);
 					frame.setVisible(true);
 					frame.setLocationRelativeTo(null);
