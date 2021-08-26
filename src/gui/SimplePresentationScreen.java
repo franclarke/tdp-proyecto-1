0da53dbf66aa524bbdd9f9eecad6dff8051f9e99
franclarke <franclarke00@hotmail.com>
2021-08-25 22:26:11 -0300

    Jar ejecutable

--- src/gui/SimplePresentationScreen.java
+++ src/gui/SimplePresentationScreen.java
@@ -1,14 +1,22 @@
 package gui;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 import entities.Student;
 import javax.swing.JTabbedPane;
+import java.awt.Toolkit;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+import java.awt.Font;
+import java.awt.Color;
 
 @SuppressWarnings("serial")
 public class SimplePresentationScreen extends JFrame {
@@ -17,17 +25,24 @@ public class SimplePresentationScreen extends JFrame {
 	private JPanel tabInformation;
 	private JTabbedPane tabbedPane;
 	private Student studentData;
+	private JTextField textLU;
+	private JTextField textApellido;
+	private JTextField textNombre;
+	private JTextField textEmail;
+	private JTextField textGithub;
+	private JLabel lblTime;
+	
 
 	public SimplePresentationScreen(Student studentData) {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(SimplePresentationScreen.class.getResource("/images/tdp.png")));
 		this.studentData = studentData;
 		
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setForeground(Color.BLACK);
 		
-		setTitle("TdP-DCIC-UNS 2021 :: Pantalla de presentación");
+		setTitle("TdP-DCIC-UNS 2021 :: Pantalla de presentacion");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(new Dimension(615, 250));
+		setSize(new Dimension(615, 275));
 		setResizable(false);
 		setContentPane(contentPane);
 		
@@ -37,9 +52,94 @@ public class SimplePresentationScreen extends JFrame {
 	private void init() {
 		// Tabbed Pane to student personal data
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBorder(null);
+		tabbedPane.setFont(new Font("SansSerif", Font.BOLD, 12));
+		tabbedPane.setBounds(5, 5, 430, 200);
 		tabInformation = new JPanel();
-		tabInformation.setPreferredSize(new Dimension(425, 275));
-		tabbedPane.addTab("Información del alumno", null, tabInformation, "Muestra la información declarada por el alumno");
-		contentPane.add(tabbedPane, BorderLayout.WEST);
+		tabInformation.setPreferredSize(new Dimension(425, 245));
+		tabbedPane.addTab("Informacion del alumno", null, tabInformation, "Muestra la informacion declarada por el alumno");
+		tabInformation.setLayout(null);
+		
+		textLU = new JTextField();
+		textLU.setBounds(85, 10, 330, 19);
+		textLU.setText(String.valueOf(studentData.getId()));
+		textLU.setEditable(false);
+		textLU.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		textLU.setToolTipText("");
+		tabInformation.add(textLU);
+		textLU.setColumns(10);
+		
+		textApellido = new JTextField();
+		textApellido.setBounds(85, 39, 330, 19);
+		textApellido.setText(studentData.getLastName());
+		textApellido.setEditable(false);
+		textApellido.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		textApellido.setColumns(10);
+		tabInformation.add(textApellido);
+		
+		textNombre = new JTextField();
+		textNombre.setBounds(85, 68, 330, 19);
+		textNombre.setText(studentData.getFirstName());
+		textNombre.setEditable(false);
+		textNombre.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		textNombre.setColumns(10);
+		tabInformation.add(textNombre);
+		
+		textEmail = new JTextField();
+		textEmail.setBounds(85, 97, 330, 19);
+		textEmail.setText(studentData.getMail());
+		textEmail.setEditable(false);
+		textEmail.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		textEmail.setColumns(10);
+		tabInformation.add(textEmail);
+		
+		textGithub = new JTextField();
+		textGithub.setBounds(85, 126, 330, 19);
+		textGithub.setText(studentData.getGithubURL());
+		textGithub.setEditable(false);
+		textGithub.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		textGithub.setColumns(10);
+		tabInformation.add(textGithub);
+		
+		JLabel lblLU = new JLabel("LU");
+		lblLU.setBounds(10, 10, 55, 19);
+		lblLU.setFont(new Font("SansSerif", Font.BOLD, 12));
+		tabInformation.add(lblLU);
+		
+		JLabel lblApellido = new JLabel("Apellido");
+		lblApellido.setBounds(10, 39, 55, 19);
+		lblApellido.setFont(new Font("SansSerif", Font.BOLD, 12));
+		tabInformation.add(lblApellido);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(10, 68, 55, 19);
+		lblNombre.setFont(new Font("SansSerif", Font.BOLD, 12));
+		tabInformation.add(lblNombre);
+		
+		JLabel lblEmail = new JLabel("E-mail");
+		lblEmail.setBounds(10, 97, 55, 19);
+		lblEmail.setFont(new Font("SansSerif", Font.BOLD, 12));
+		tabInformation.add(lblEmail);
+		
+		JLabel lblGithub = new JLabel("Github URL");
+		lblGithub.setBounds(10, 126, 66, 19);
+		lblGithub.setFont(new Font("SansSerif", Font.BOLD, 12));
+		tabInformation.add(lblGithub);
+		
+        JLabel lbl_img = new JLabel();
+        lbl_img.setIcon(new ImageIcon(SimplePresentationScreen.class.getResource(studentData.getPathPhoto())));
+        lbl_img.setBounds(445, 44, 146, 146);
+        contentPane.add(lbl_img);
+        
+        lbl_img.setVisible(true);
+		
+		DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("HH:mm:ss");
+		contentPane.setLayout(null);
+		contentPane.add(tabbedPane);
+		lblTime = new JLabel("Esta ventana fue generada el "+ dtf1.format(LocalDateTime.now()) + " a las: "+dtf2.format(LocalDateTime.now()));
+		lblTime.setBounds(15, 210, 405, 19);
+		contentPane.add(lblTime);
+		lblTime.setFont(new Font("SansSerif", Font.BOLD, 12));
 	}
 }
--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,12 +1,24 @@
 package launcher;
 
-public class Launcher {
+import entities.Student;
+import gui.SimplePresentationScreen;
+
+public class Launcher{
+	
+	private static Student stud = new Student(120547, "Clarke", "Francisco", "franclarke00@hotmail.com", "https://github.com/franclarke", "/images/foto.png");
+	
 	public static void main(String [] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-            	
+            	try {
+            		SimplePresentationScreen frame = new SimplePresentationScreen(stud);
+					frame.setVisible(true);
+					frame.setLocationRelativeTo(null);
+            	}catch (Exception e) {
+					e.printStackTrace();
+				}
             }
         });
     }
